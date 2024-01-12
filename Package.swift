d0f469bf60f54739fa876c224b23682a931cb83c
bragelbytes <joebragel@gmail.com>
2024-01-11 17:57:47 -0800

    update swift-log dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["FileLogging"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
     ],
     targets: [
         .target(
